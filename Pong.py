8c6c12cbea582cde3eea0f4f2c77764323aaa6c8
Alain-Rostomyan <101672935+Alain-Rostomyan@users.noreply.github.com>
2023-12-07 10:16:22 +0100

    Pong v3
    Fixed paddle movement for 2-player mode
    1 player mode still needs paddle movement fix

--- Pong.py
+++ Pong.py
@@ -6,7 +6,7 @@ screen = turtle.Screen()
 screen.title("Pong -- Alain, Millan, Alienor, Toscane")
 screen.bgcolor("black")
 screen.setup(width=600, height=400)
-screen.tracer(0) #Turn on manual updates of the screen -> Makes the game quicker
+screen.tracer(0)  # Turn on manual updates of the screen -> Makes the game quicker
 ### Could we make it bigger ?
 
 # Create the title and choose mode texts
@@ -39,140 +39,163 @@ ai_text.hideturtle()
 ai_text.goto(0, -60)
 ai_text.write("AI Mode", align="center", font=("Courier", 18, "normal"))
 
+w_pressed = False
+s_pressed = False
+up_pressed = False
+down_pressed = False
+
 
 def setup_2_players():
-    # Set up the window
-    screen = turtle.Screen()
-    screen.title("Pong-- Alain, Millan, Alienor, Toscane")
-    screen.bgcolor("black")
-    screen.setup(width=600, height=400)
-    ###again, bigger ?
-
     # Create paddles and ball
-    # Player 1 paddle
     player1_paddle = turtle.Turtle()
     player1_paddle.shape("square")
-    player1_paddle.color("white") 
-    player1_paddle.shapesize(stretch_wid=4, stretch_len=0.5)  # Adjust the size to make it thinner and shorter
+    player1_paddle.color("white")
+    player1_paddle.shapesize(stretch_wid=3, stretch_len=0.5)  # Adjust the size to make it thinner and shorter
     player1_paddle.penup()
     player1_paddle.goto(-250, 0)
 
-    # Player 2 paddle
     player2_paddle = turtle.Turtle()
     player2_paddle.shape("square")
     player2_paddle.color("white")
-    player2_paddle.shapesize(stretch_wid=4, stretch_len=0.5)  # Adjust the size to make it thinner and shorter
+    player2_paddle.shapesize(stretch_wid=3, stretch_len=0.5)  # Adjust the size to make it thinner and shorter
     player2_paddle.penup()
     player2_paddle.goto(250, 0)
 
-    # Ball
     ball = turtle.Turtle()
     ball.shape("circle")
     ball.color("white")
     ball.shapesize(stretch_wid=0.75, stretch_len=0.75)  # Adjust the size to make it smaller
     ball.penup()
     ball.goto(0, 0)
-    # Initial ball speed (adjust as needed)
-    ball_speed_x = 0.8
-    ball_speed_y = 0.8
-    ### Could we find a way to make it quicker as the levels pass by ?
+
+    # Set initial ball speed (adjust as needed)
+    ball_speed_x = 1
+    ball_speed_y = 1
 
     # Initialize scores
     score_player1 = 0
     score_player2 = 0
 
-    # Set up the score display
+    # Set up the font for displaying scores
     score_display = turtle.Turtle()
     score_display.color("white")
     score_display.penup()
     score_display.hideturtle()
     score_display.goto(0, 160)
-    #score_display.write("0 - 0", align="center", font=("Courier", 24, "bold"))
 
     # Draw dashed line
     dashed_line = turtle.Turtle()
     dashed_line.color("white")
     dashed_line.penup()
     dashed_line.hideturtle()
-    ### What is this for ?
 
-    #Set up functions for moving the paddles
-    # Function to move player 1 paddle up
-    def move_paddle1_up():
-        y = player1_paddle.ycor()
-        if y < 160:
-            player1_paddle.sety(y + 25)
-
-    # Function to move player 1 paddle down
-    def move_paddle1_down():
-        y = player1_paddle.ycor()
-        if y > -160:
-            player1_paddle.sety(y - 25)
-
-    # Function to move player 2 paddle up
-    def move_paddle2_up():
-        y = player2_paddle.ycor()
-        if y < 160:
-            player2_paddle.sety(y + 25)
-
-    # Function to move player 2 paddle down
-    def move_paddle2_down():
-        y = player2_paddle.ycor()
-        if y > -160:
-            player2_paddle.sety(y - 25)
-
-    # Keyboard input for controls
+    # Set up the keyboard bindings for continuous movement
     screen.listen()
 
-    # Player 1 controls
-    screen.onkeypress(move_paddle1_up, "s")
-    screen.onkeypress(move_paddle1_down, "w")
 
-    # Player 2 controls
-    screen.onkeypress(move_paddle2_up, "Up")
-    screen.onkeypress(move_paddle2_down, "Down")
+    # player 1 controls
+    def stop_move_paddle1_up():
+        global w_pressed
+        w_pressed = False
 
-    # Set animation speed and turn on manual screen updates -> makes game quicker
+    def stop_move_paddle1_down():
+        global s_pressed
+        s_pressed = False
+
+    def move_paddle1_up():
+        if player1_paddle.ycor() < 160:
+            player1_paddle.sety(player1_paddle.ycor() + 5)
+
+    def move_paddle1_down():
+        if player1_paddle.ycor() > -150:
+            player1_paddle.sety(player1_paddle.ycor() - 5)
+
+    # player2 controls
+    def stop_move_paddle2_up():
+        global up_pressed
+        up_pressed = False
+
+    def stop_move_paddle2_down():
+        global down_pressed
+        down_pressed = False
+
+    def move_paddle2_up():
+        if player2_paddle.ycor() < 160:
+            player2_paddle.sety(player2_paddle.ycor() + 5)
+
+    def move_paddle2_down():
+        if player2_paddle.ycor() > -150:
+            player2_paddle.sety(player2_paddle.ycor() - 5)
+
+
+    def press_w():
+        global w_pressed
+        w_pressed = True
+
+    def press_s():
+        global s_pressed
+        s_pressed = True
+
+    def press_up():
+        global up_pressed
+        up_pressed = True
+
+    def press_down():
+        global down_pressed
+        down_pressed = True
+
+    turtle.listen()
+    turtle.onkeypress(press_w, "w")
+    turtle.onkeyrelease(stop_move_paddle1_up, "w")
+    turtle.onkeypress(press_s, "s")
+    turtle.onkeyrelease(stop_move_paddle1_down, "s")
+    turtle.onkeypress(press_up, "Up")
+    turtle.onkeyrelease(stop_move_paddle2_up, "Up")
+    turtle.onkeypress(press_down, "Down")
+    turtle.onkeyrelease(stop_move_paddle2_down, "Down")
+
+    # Set animation speed and turn on screen updates
     screen.tracer(0)
     turtle.speed(0)
 
     # Main game loop
     while True:
+
+        if w_pressed:
+            move_paddle1_up()
+        if s_pressed:
+            move_paddle1_down()
+        if up_pressed:
+            move_paddle2_up()
+        if down_pressed:
+            move_paddle2_down()
+
         # Draw dashed line
         dashed_line.clear()
         dashed_line.goto(0, -200)
         dashed_line.pendown()
         dashed_line.setheading(90)
-        ### I am lost about the dashed line
-        
         for _ in range(20):
             dashed_line.forward(20)
             dashed_line.penup()
             dashed_line.forward(20)
             dashed_line.pendown()
-        ###SOS I DON'T UNDERSTAND
 
         # Move the ball
         ball.setx(ball.xcor() + ball_speed_x)
         ball.sety(ball.ycor() + ball_speed_y)
 
-        # Ball collision with upper and lower walls
-        if ball.ycor()>190:
-            ball.sety(190)
-            ball_yspeed *= -1
-
-        if ball.ycor()<-190:
-            ball.sety(-190)
-            ball_yspeed *= -1
-        
+        # Ball collision with walls
+        if ball.ycor() > 190 or ball.ycor() < -190:
+            ball_speed_y *= -1
 
         # Ball collision with paddles
         if (
                 (240 < ball.xcor() < 250)
-                and (player2_paddle.ycor() + 20 > ball.ycor() > player2_paddle.ycor() - 20)
+                and (player2_paddle.ycor() + 35 > ball.ycor() > player2_paddle.ycor() - 35)
         ) or (
                 (-240 > ball.xcor() > -250)
-                and (player1_paddle.ycor() + 20 > ball.ycor() > player1_paddle.ycor() - 20)
+                and (player1_paddle.ycor() + 35 > ball.ycor() > player1_paddle.ycor() - 35)
         ):
             ball_speed_x *= -1
 
@@ -193,18 +216,18 @@ def setup_2_players():
 
         # Update the score display
         score_display.clear()
-        score_display.write(f"{score_player1} - {score_player2}", align="center", font=("Courier", 24, "bold"))
+        score_display.write(f"{score_player1} - {score_player2}", align="center", font=("Courier", 24, "normal"))
 
         # Update the screen
         screen.update()
-        ### Why ?
 
         # Pause briefly to control the speed of the game
         turtle.delay(10)
 
-        # Check wether to exit the game
+        # Check whether to exit the game
         if screen.getcanvas().winfo_reqheight() <= 1:  # window is closed
             break
+
 
 def setup_1_player():
     import turtle
@@ -262,16 +285,21 @@ def setup_1_player():
     screen.listen()
 
     # Function to move player 1 paddle up
-    def move_paddle1_up():
-        y = player1_paddle.ycor()
-        if y < 160:
-            player1_paddle.sety(y + 25)
+    def stop_move_paddle1_up():
+        global w_pressed
+        w_pressed = False
 
-    # Function to move player 1 paddle down
+    def stop_move_paddle1_down():
+        global s_pressed
+        s_pressed = False
+
+    def move_paddle1_up():
+        if player1_paddle.ycor() < 160:
+            player1_paddle.sety(player1_paddle.ycor() + 5)
+
     def move_paddle1_down():
-        y = player1_paddle.ycor()
-        if y > -150:
-            player1_paddle.sety(y - 25)
+        if player1_paddle.ycor() > -150:
+            player1_paddle.sety(player1_paddle.ycor() - 5)
 
     # Player 1 controls
     screen.onkeypress(move_paddle1_up, "w")
@@ -283,6 +311,12 @@ def setup_1_player():
 
     # Main game loop
     while True:
+        if w_pressed:
+            move_paddle1_up()
+        if s_pressed:
+            move_paddle1_down()
+
+
         # Draw dashed line
         dashed_line.clear()
         dashed_line.goto(0, -200)
@@ -310,11 +344,11 @@ def setup_1_player():
 
         # Ball collision with paddles
         if (
-                (ball.xcor() > 240 and ball.xcor() < 250)
-                and (ball.ycor() < player2_paddle.ycor() + 20 and ball.ycor() > player2_paddle.ycor() - 20)
+                (240 < ball.xcor() < 250)
+                and (player2_paddle.ycor() + 35 > ball.ycor() > player2_paddle.ycor() - 35)
         ) or (
-                (ball.xcor() < -240 and ball.xcor() > -250)
-                and (ball.ycor() < player1_paddle.ycor() + 20 and ball.ycor() > player1_paddle.ycor() - 20)
+                (-240 > ball.xcor() > -250)
+                and (player1_paddle.ycor() + 35 > ball.ycor() > player1_paddle.ycor() - 35)
         ):
             ball_speed_x *= -1
 
@@ -368,6 +402,7 @@ def on_click(x, y):
 
         # Set up the game for one player
         setup_1_player()
+
 
 # Bind click events to screen
 screen.onscreenclick(on_click)
